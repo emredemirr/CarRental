8c5eb56d88c2a2c3f02027d2b27e99254213883e
Emre Demir <emreedemir@yahoo.com>
2021-04-04 16:12:59 +0300

    All business classes have been refactored according to the result structure.

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -10,5 +10,12 @@ namespace Business.Constants
         public static string CarNameInvalid = "Girilen bilgiler hatalıdır.";
         public static string CarDeleted = "Araç Silindi.";
         public static string CarUpdated = "Araç Güncellendi.";
+        public static string BrandAdded = "Marka Eklendi.";
+        public static string BrandDeleted = "Marka Silindi.";
+        public static string BrandUpdated = "Marka Güncellendi.";
+        public static string ColorAdded = "Renk Eklendi.";
+        public static string ColorDeleted = "Renk Silindi.";
+        public static string ColorUpdated = "Renk Güncellendi.";
+
     }
 }
