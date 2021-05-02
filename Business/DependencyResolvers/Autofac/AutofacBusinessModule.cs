bae07972e6770624e01807b1b74b9bd2e6d36c98
Emre Demir <emreedemir@yahoo.com>
2021-05-02 12:53:46 +0300

    Autofac support added

---
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,31 @@
+﻿using Autofac;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Abstract.EntityFramework;
+using DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule: Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<CarManager>().As<ICarService>().SingleInstance();
+            builder.RegisterType<EfCarDal>().As<ICarDal>().SingleInstance();
+            builder.RegisterType<BrandManager>().As<IBrandService>().SingleInstance();
+            builder.RegisterType<EfBrandDal>().As<IBrandDal>().SingleInstance();
+            builder.RegisterType<ColorManager>().As<IColorService>().SingleInstance();
+            builder.RegisterType<EfColorDal>().As<IColorDal>().SingleInstance();
+            builder.RegisterType<CustomerManager>().As<ICustomersService>().SingleInstance();
+            builder.RegisterType<EfCustomersDal>().As<ICustomersDal>().SingleInstance();
+            builder.RegisterType<RentalManager>().As<IRentalsService>().SingleInstance();
+            builder.RegisterType<EfRentalsDal>().As<IRentalsDal>().SingleInstance();
+            builder.RegisterType<UserManager>().As<IUsersService>().SingleInstance();
+            builder.RegisterType<EfUsersDal>().As<IUsersDal>().SingleInstance();
+        }
+    }
+}
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -31,18 +31,18 @@ namespace WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddSingleton<ICarService, CarManager>();
-            services.AddSingleton<ICarDal, EfCarDal>();
-            services.AddSingleton<IBrandService, BrandManager>();
-            services.AddSingleton<IBrandDal, EfBrandDal>();
-            services.AddSingleton<IColorService, ColorManager>();
-            services.AddSingleton<IColorDal, EfColorDal>();
-            services.AddSingleton<ICustomersService, CustomerManager>();
-            services.AddSingleton<ICustomersDal, EfCustomersDal>();
-            services.AddSingleton<IRentalsService, RentalManager>();
-            services.AddSingleton<IRentalsDal, EfRentalsDal>();
-            services.AddSingleton<IUsersService, UserManager>();
-            services.AddSingleton<IUsersDal, EfUsersDal>();
+            //services.AddSingleton<ICarService, CarManager>();
+            //services.AddSingleton<ICarDal, EfCarDal>();
+            //services.AddSingleton<IBrandService, BrandManager>();
+            //services.AddSingleton<IBrandDal, EfBrandDal>();
+            //services.AddSingleton<IColorService, ColorManager>();
+            //services.AddSingleton<IColorDal, EfColorDal>();
+            //services.AddSingleton<ICustomersService, CustomerManager>();
+            //services.AddSingleton<ICustomersDal, EfCustomersDal>();
+            //services.AddSingleton<IRentalsService, RentalManager>();
+            //services.AddSingleton<IRentalsDal, EfRentalsDal>();
+            //services.AddSingleton<IUsersService, UserManager>();
+            //services.AddSingleton<IUsersDal, EfUsersDal>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
