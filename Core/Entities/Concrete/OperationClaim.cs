be333515baf8b3714520ab14d805f3cb061d9c4f
Emre Demir <yzbdemir@outlook.com>
2021-05-18 13:33:08 +0300

    SecurityOperation - HashingHelper added

---
+++ Core/Entities/Concrete/OperationClaim.cs
@@ -0,0 +1,12 @@
+﻿using Entities.Concrete;
+
+namespace Core.Entities.Concrete
+{
+    public class OperationClaim:IEntity
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+    }
+}
---
+++ Core/Entities/Concrete/UserOperationClaim.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Concrete;
+
+namespace Core.Entities.Concrete
+{
+    public class UserOperationClaim:IEntity
+    {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public int OperationClaimId { get; set; }
+
+    }
+}
